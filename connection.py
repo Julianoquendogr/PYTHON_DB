c3e36b684ec4baeba60ecaf77b5bd0974ca3befe
Julian <julian.quendogr@gmail.com>
2019-11-03 10:28:37 -0500

    Insert into Zonas table

---
+++ connection.py
@@ -0,0 +1,31 @@
+import xlrd
+import pyodbc
+
+book = xlrd.open_workbook(r'C:\\Users\\Julian\\Downloads\\TABLAS_PRUEBA_ING_DATOS.xlsx')
+sheet = book.sheet_by_name('cat_zona')
+
+conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server=DESKTOP-8LT275E\SQLEXPRESS;'
+                      'Database=DB_PYTHON;'
+                      'Trusted_Connection=yes;')
+
+cursor = conn.cursor()
+
+for i in range(1, sheet.nrows):
+
+    Cod_Pais_Region_Zona = sheet.cell(i, 0).value
+    Desc_Zona = sheet.cell(i, 1).value
+    Desc_Region = sheet.cell(i, 2).value
+    Desc_Pais = sheet.cell(i, 3).value
+
+    query = '''INSERT INTO ZONA (Cod_Pais_Region_Zona, Desc_Zona,Desc_Region, Desc_Pais) VALUES (?,?,?,?)'''
+    values = (Cod_Pais_Region_Zona, Desc_Zona, Desc_Region, Desc_Pais)
+    cursor.execute(query, values)
+    conn.commit()
+
+print("Se insertaron : " + str(sheet.nrows - 1) + " registros en la tabla ZONA")
+cursor.close()
+conn.close()
+
+########################################################################################################
+
